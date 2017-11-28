f446ab03ac3af2662ff43db6336d48f44a3ed20c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-11-27 22:30:11 -0500

    initial

---
+++ cypress/integration/spec.js
@@ -0,0 +1,74 @@
+// our Codepen
+const url = 'https://codepen.io/bahmutov/full/ZaMxgz/'
+const iframeUrl = 'https://s.codepen.io/bahmutov/fullpage/ZaMxgz'
+
+describe('HyperApp Counter Codepen', () => {
+  beforeEach(() => {
+    cy
+      .request({
+        method: 'GET',
+        url: iframeUrl,
+        headers: {
+          Referer: url,
+          accept: 'text/html',
+          'content-type': 'text/html'
+        }
+      })
+      .its('body')
+      .then(html => {
+        cy.window().its('document').then(document => {
+          document.write(html)
+          document.close()
+        })
+      })
+    cy.get('main').should('be.visible')
+  })
+
+  const getCount = () => cy.get('main').find('h1')
+
+  // NOTE: while it looks like buttons have regular ASCII "+" and "-"
+  // in reality these are Unicode symbols ＋ and ー
+  const getPlus = () => cy.get('main').contains('button', '＋')
+  const getMinus = () => cy.get('main').contains('button', 'ー')
+
+  it('increments and decrements via UI', () => {
+    getPlus().click()
+    getPlus().click()
+    getPlus().click()
+    getMinus().click()
+    getCount().contains(2).should('be.visible')
+  })
+
+  it('has decrement button disabled initially', () => {
+    getMinus().should('be.disabled')
+  })
+
+  it('enables decrement button for positive numbers', () => {
+    getPlus().click()
+    getMinus().should('not.be.disabled')
+
+    getMinus().click()
+    getMinus().should('be.disabled')
+  })
+
+  const getApp = () => cy.window().its('_app')
+
+  it("can drive DOM via App's actions", () => {
+    getApp().then(actions => {
+      actions.up()
+      actions.up()
+      actions.up()
+      actions.down()
+      getCount().contains(2).should('be.visible')
+    })
+  })
+
+  it('can even drive App into invalid state', () => {
+    getApp().then(actions => {
+      actions.down()
+      actions.down()
+      getCount().contains(-2).should('be.visible')
+      getMinus().should('be.disabled')
+    })
+  })
+})
